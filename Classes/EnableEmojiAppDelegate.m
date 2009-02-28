e37754633f96a0679935206f471b0d1c1bd5fca9
Yoshimasa Niwa <niw@niw.at>
2009-02-28 17:33:43 +0000

    * Remove 0byte plist
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/iphone/EnableEmoji@2332 c432a1ab-ec09-0410-8e5f-fb625be92370

--- Classes/EnableEmojiAppDelegate.m
+++ Classes/EnableEmojiAppDelegate.m
@@ -4,9 +4,26 @@
 @synthesize window;
 @synthesize viewController;
 
+static NSString *preferencesBasePath = @"/private/var/mobile/Library/Preferences/";
+
 - (void)applicationDidFinishLaunching:(UIApplication *)application {    
 	[window addSubview:viewController.view];
 	[window makeKeyAndVisible];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+	NSString *file;
+	NSDirectoryEnumerator *dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:preferencesBasePath];
+	while(file = [dirEnum nextObject]) {
+		if([file hasPrefix:@"com.apple.Preferences.plist."]) {
+			NSString *path = [preferencesBasePath stringByAppendingPathComponent:file];
+			NSDictionary *attributes = [[NSFileManager defaultManager] fileAttributesAtPath:path traverseLink:YES];
+			if([attributes fileSize] == 0) {
+				NSError *error;
+				[[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+			}
+		}
+	}
 }
 
 - (void)dealloc {
